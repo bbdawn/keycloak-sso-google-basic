7450262d1d2f5c724be1721331e6e558ab24e97f
이주현 <leejoohyun@ijuhyeon-ui-MacBookPro.local>
2022-12-20 00:18:52 +0900

    Role 터미널 curl test 성공

--- src/main/java/com/example/keycloakproject/config/KeycloakSecurityConfig.java
+++ src/main/java/com/example/keycloakproject/config/KeycloakSecurityConfig.java
@@ -1,9 +1,6 @@
 package com.example.keycloakproject.config;
 
 import org.keycloak.adapters.KeycloakConfigResolver;
-import org.keycloak.adapters.KeycloakDeployment;
-import org.keycloak.adapters.KeycloakDeploymentBuilder;
-import org.keycloak.adapters.spi.HttpFacade;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
@@ -19,30 +16,24 @@ import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
-import java.io.InputStream;
-
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
 public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
-//    SpringSecurity 설정과 유사하다
-//    간단하게 /app* 요청은 permitAll이고, 그 외에는 모두 인증 절차를 거쳐야한다.
-//    즉 /app* 외에는 모두 Keycloak이 작동을 해야 한다.
-
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        super.configure(http);
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/test/permitAll").permitAll() //누구든지
-//                .antMatchers("/test/authenticated").authenticated() //인증한 사람만
-//                //Keycloak -> My_Realm -> Client -> My_Client -> Roles
-//                .antMatchers("/test/user").hasAnyRole("USER") //USER Role을 가지고 있는 사람만
-//                .antMatchers("/test/user").hasAnyRole("ADMIN") //ADMIN Role을 가지고 있는 사람만
-//                .anyRequest()
-//                .permitAll();
-//        http.csrf().disable();
-//    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http
+                .authorizeRequests()
+                .antMatchers("/test/permitAll").permitAll()
+                .antMatchers("/test/authenticated").authenticated()
+                .antMatchers("/test/user").hasAnyRole("USER")
+                .antMatchers("/test/admin").hasAnyRole("ADMIN")
+                .anyRequest()
+                .permitAll();
+        http.csrf().disable();
+    }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
@@ -54,32 +45,11 @@ public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter
     @Bean
     @Override
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(
-                new SessionRegistryImpl());
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
-        http.authorizeRequests()
-                .antMatchers("/app*").permitAll()
-                .anyRequest().authenticated();
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
     }
 
     @Bean
-    public KeycloakConfigResolver keycloakConfigResolver() {
-        return new KeycloakConfigResolver() {
-
-            private KeycloakDeployment keycloakDeployment;
-            @Override
-            public KeycloakDeployment resolve(HttpFacade.Request facade) {
-                if (keycloakDeployment != null) {
-                    return keycloakDeployment;
-                }
-
-                InputStream configInputStream = getClass().getResourceAsStream("/keycloak.json");
-                return KeycloakDeploymentBuilder.build(configInputStream);
-            }
-        };
+    public KeycloakConfigResolver KeycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
     }
 }
--- src/main/java/com/example/keycloakproject/controller/TestController.java
+++ src/main/java/com/example/keycloakproject/controller/TestController.java
@@ -9,18 +9,34 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("")
+@RequestMapping("/test")
 public class TestController {
 
-    @RequestMapping("/hello")
-    public String hello() {
-        return "Hello KeyCloak!";
+    @RequestMapping(value = "/permitAll", method = RequestMethod.GET)
+    public ResponseEntity<String> permitAll() {
+        return ResponseEntity.ok("누구나 접근이 가능합니다.\n");
     }
 
-    @RequestMapping("/app1")
-    public String tracingTest() {
-        return "This is permitAll!";
+    @RequestMapping(value = "/authenticated", method = RequestMethod.GET)
+    public ResponseEntity<String> authenticated(@RequestHeader String Authorization) {
+        log.debug(Authorization);
+        return ResponseEntity.ok("로그인한 사람 누구나 가능합니다.\n");
     }
 
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public ResponseEntity<String> user(@RequestHeader String Authorization) {
+        log.debug(Authorization);
+        return ResponseEntity.ok("user 가능합니다.\n");
+    }
+
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public ResponseEntity<String> admin(@RequestHeader String Authorization) {
+        log.debug(Authorization);
+        return ResponseEntity.ok("admin 가능합니다.\n");
+    }
 
 }
+
+
+
+
